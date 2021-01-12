e5752d8a780d6104f0a4ad96f5a1cbd429e65403
Batin Eryilmaz <batineryilmaz@icloud.com>
2021-01-13 01:43:43 +0300

    Add homepage icon

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.scss";
 import { Questions } from "./Questions/Questions.js";
 import { WithInfo } from "./Hocs/With-info";
+import Richie from "./Assets/richie.svg";
 
 function App() {
   return (
     <div className="App">
       <WithInfo >
+        <h1 className="heading">Hangi aşı grubundayım?</h1>
         <Questions />
+        <img className="homepage-icon" src={Richie} alt="Homepage Icon" />
       </WithInfo>
     </div>
   );
