a08046e4330cb03da8d38a22e55e81afd2e7a3c5
alihany <alihanyiilmaz@gmail.com>
2021-01-15 00:27:01 +0300

    Add link to redirect to info page and Saglik Bakanligi Asi Table
    I add a tag to redirect to Info page (which is main page) if user wants to try again with different informations.
    I add a link to redirect Saglik Bakanligi Asi Table if user wants to know more information about groups and orders

--- src/Result/Result.js
+++ src/Result/Result.js
@@ -14,47 +14,77 @@ const Result = () => {
   }, [])
 
   const sendEvent = (event) => {
-    window.send({ UserGroup: `${group}. group`, Vote: event })
-    localStorage.setItem('voted', 'true');
+    window.send({ UserGroup: `${group}. group`, Vote: event });
+    localStorage.setItem("voted", "true");
     setShow(false);
   };
 
-  const redicectToTwitter = () => {
+  const redirectToTwitter = () => {
     line === "" ?
     window.open(`https://twitter.com/intent/tweet?text=${group}.%20a%C5%9F%C4%B1%20grubunday%C4%B1m&url=https://hangiasigrubundayim.com/${group}.html`, "_blank")
     :
     window.open(`https://twitter.com/intent/tweet?text=${group}.%20a%C5%9F%C4%B1%20grubunda%20${line}%20s%C4%B1ras%C4%B1nday%C4%B1m&url=https://hangiasigrubundayim.com/${group}.html`, "_blank")
   }
 
+  const redirectToOfficial = () => {
+    window.open(
+      `https://covid19asi.saglik.gov.tr/TR-77707/covid-19-asisi-uygulanacak-kisiler.html`,
+      "_blank"
+    );
+  };
+
   return (
     <div className="result">
       <div className="result-heading">
-        {
-          line === "" ?
-            <h1 className="result-text"><span className="important">{group}.</span> aşı grubundasın.</h1>
-            :
-            <h1 className="result-text"><span className="important">{group}.</span> aşı grubunda <span className="important">{line}</span> sırasındasın.</h1>
-        }
+        {line === "" ? (
+          <h1 className="result-text">
+            <span className="important" onClick={redirectToOfficial}>
+              {group}.
+            </span>{" "}
+            aşı grubundasın.
+          </h1>
+        ) : (
+          <h1 className="result-text">
+            <span className="important" onClick={redirectToOfficial}>
+              {group}.
+            </span>{" "}
+            aşı grubunda{" "}
+            <span className="important" onClick={redirectToOfficial}>
+              {line}
+            </span>{" "}
+            sırasındasın.
+          </h1>
+        )}
       </div>
-      { show && <div className="separator" />}
-      { show &&
-        (<div className="survey">
+      {show && <div className="separator" />}
+      {show && (
+        <div className="survey">
           <h2>Aşı olmayı düşünüyor musun?</h2>
           <div className="buttons">
             <button className="left" onClick={() => sendEvent("Evet")}>
               Evet
-                </button>
+            </button>
             <button className="mid" onClick={() => sendEvent("Kararsizim")}>
               Karasızım
-                </button>
+            </button>
             <button className="right" onClick={() => sendEvent("Hayir")}>
               Hayır
-                </button>
+            </button>
           </div>
-        </div>)
-      }
-      { show && <p className="result-info">*Anket verileri <strong className="tooltip"> anonim</strong> şekilde saklanmaktadır.</p>}
-      <div className="twitter" onClick={redicectToTwitter}>{"Twitter'da Paylaş"}</div>
+        </div>
+      )}
+      {show && (
+        <p className="result-info">
+          *Anket verileri <strong className="tooltip"> anonim</strong> şekilde
+          saklanmaktadır.
+        </p>
+      )}
+      <div className="twitter button" onClick={redirectToTwitter}>
+        {"Twitter'da Paylaş"}
+      </div>
+      <a className="back button" href="/">
+        {"Geri Dön"}
+      </a>
     </div>
   );
 };
