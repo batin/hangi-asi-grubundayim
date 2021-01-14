ae84e46fe0a5d4ed22f5672321c84517daeb86a2
Fexyler <batuhanisildak@sabanciuniv.edu>
2021-01-14 20:11:03 +0300

    some fixes 🧨
    I've changed these :
    - created a router page inside of a directory called "utils"
    - added padding to twitter and result boxes but looks like it's not responsive right now, I think we might think responsive design for this project because in future it will be hard
    - changed "altı" keyword to "arası" but not sure what was your purpose we can discuss it ! :)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import React, { useEffect } from "react";
 import "./App.scss";
-import { WithInfo } from "./Hocs/With-info";
-import Richie from "./Assets/richie.png";
-import twitter from "./Assets/twitter.webp";
-import { Content } from "./Content/Content";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import Router from "./utils/Router";
+
+
 function App() {
 
   useEffect(() => {
@@ -17,24 +11,7 @@ function App() {
 
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          <Route path="/">
-            <WithInfo>
-              <h1 className="heading">Hangi aşı grubundayım?</h1>
-              <Content />
-              <img className="homepage-icon" src={Richie} alt="Homepage Icon" />
-            </WithInfo>
-            <p className="info">*Hesaplamalar tahmini veriler ile yapılmaktadır.</p>
-            <a href="https://twitter.com/btnerylmz" target="_blank" rel="noreferrer"><img className="twitter-icon" src={twitter} alt="Twitter Icon" /></a>
-          </Route>
-          <Route path="/2.html">
-          </Route>
-          <Route path="/1.html">
-          </Route>
-        </Switch>
-      </Router>
-
+      <Router />
     </div>
   );
 }
